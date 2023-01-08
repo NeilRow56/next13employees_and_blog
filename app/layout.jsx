61c676626839401de6316e7dec99c28be15228c7
Neil <neilrowland56@yahoo.com>
2023-01-08 17:21:05 +0000

    first commit basic page layou and tailwind

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,12 @@
-import './globals.css'
+"use client";
+import "./globals.css";
+import Navbar from "./components/mainLayout/Navbar";
+import Footer from "./components/mainLayout/Footer";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+// import { SessionProvider } from "next-auth/react";
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children, session }) {
   return (
     <html lang="en">
       {/*
@@ -8,7 +14,13 @@ export default function RootLayout({ children }) {
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+
+      <body>
+        <ToastContainer position="bottom-center" limit={1} />
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+      </body>
     </html>
-  )
+  );
 }

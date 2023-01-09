c9b6c3b136603268603280231a86a5039e02f833
Neil <neilrowland56@yahoo.com>
2023-01-09 17:43:47 +0000

    Added Loading and Error boundary files

--- app/users/users.jsx
+++ app/users/users.jsx
@@ -7,8 +7,8 @@ const Users = async () => {
   return (
     <section className="fixed h-full w-1/4 bg-stone-800">
       <div className="center py-4">
-        <h2 className="mb-4 text-xl font-medium text-white">Users</h2>
-        <ul className="flex flex-col text-sm text-white">
+        <h2 className="mb-4 px-6 text-xl font-medium text-white">Users</h2>
+        <ul className="flex flex-col px-6 text-sm text-white">
           {users?.map((user) => (
             <li key={user.id}>
               <Link href={`/users/${user.id}`}>{user.name}</Link>
